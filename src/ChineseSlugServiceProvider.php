7794e56a4cf07a6bc7ac6096ef1d903657b8bde7
jiemoon <jiemoon2010@gmail.com>
2017-04-07 22:26:08 +0800

    fix

--- src/ChineseSlugServiceProvider.php
+++ src/ChineseSlugServiceProvider.php
@@ -7,13 +7,6 @@ use Illuminate\Support\ServiceProvider;
 class ChineseSlugServiceProvider extends ServiceProvider
 {
     /**
-     * 延迟加载.
-     *
-     * @var bool
-     */
-    protected $defer = true;
-
-    /**
      * Bootstrap the application services.
      *
      * @return void
@@ -33,16 +26,6 @@ class ChineseSlugServiceProvider extends ServiceProvider
         $this->app->bind('chineseslug', function() {
             return new ChineseSlug(new ChineseSlugRepository);
         });
-    }
-
-    /**
-     * Get the services provided by the provider.
-     *
-     * @return array
-     */
-    public function provides()
-    {
-        return [ChineseSlug::class];
     }
 }
 
